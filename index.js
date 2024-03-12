58656f65b9216ef5c1728f809c3815d9ad7b6aac
ashishrautela612 <ashish.rautela@techsuperiors.com>
2024-03-12 15:51:28 +0530

    adding scroll event

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".menuBtn");
+    const menuBtnBurger = document.querySelector(".menuBtnBurger");
+    const navItems = document.querySelector(".navItems");
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger=document.querySelector(".menuBtn");
-    const menuBtnBurger=document.querySelector(".menuBtnBurger");
-    const navItems=document.querySelector(".navItems");
-    
-    // hamburger.addEventListener("click",()=>{
-    //     hamburger.classList.toggle("active");
-    //     navItems.classList.toggle("active");
-    // })
-
-    menuBtnBurger.addEventListener("click",()=>{
+    menuBtnBurger.addEventListener("click", () => {
         menuBtnBurger.classList.toggle("active");
         navItems.classList.toggle("active");
-    })
+    });
 
-    document.querySelectorAll(".navItem").forEach(item =>item.addEventListener("click"),()=>{
-        hamburger.classList.remove("active");
+    document.querySelectorAll(".navItem").forEach(item => {
+        item.addEventListener("click", () => {
+            menuBtnBurger.classList.toggle("active");
+            navItems.classList.toggle("active");
+        });
+    });
+
+    window.addEventListener("scroll",()=>{
+        menuBtnBurger.classList.remove("active");
         navItems.classList.remove("active");
-
     })
 });
